15a85c7ea38dbbb7f8c21c0a4836ca338df7136c
Basliel Tegegn Dapo <BaslielTegegn@cbe.com.et>
2025-03-05 15:40:09 +0300

    ad

--- action/create_oil_lit_type.php
+++ action/create_oil_lit_type.php
@@ -7,6 +7,11 @@ if($_POST) {
 extract($_POST);
   $oil_type = $_POST['oil_type']; 
 
+  $sql = "SELECT * FROM oil_inventory WHERE oil_lit_type = '$oil_type'";
+  $result = $connect->query($sql);
+
+  if($result->num_rows == 0) {
+
   $sql = "INSERT INTO oil_inventory (oil_lit_type,amount) VALUES ('$oil_type','0')";
 
 				if($connect->query($sql) === TRUE) {
@@ -16,12 +21,16 @@ extract($_POST);
 				} 
 				else {
 					$valid['success'] = false;
-					$valid['messages'] = "Error while adding the Tanker";
+					$valid['messages'] = "Error while adding the Oil";
 					header('location:../add_oil_type.php');
 				}	
 
 	$connect->close();
 
 	echo json_encode($valid);
+
+			}else{
+				header('location:../add_oil_type.php');
+			}
  
 } 
