bf9a7ccd2f9a2f418dcb66c430e9f22d7092d2c0
Basliel Tegegn Dapo <BaslielTegegn@cbe.com.et>
2025-03-06 15:30:19 +0300

    db

--- db/gsms.sql
+++ db/gsms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2025 at 02:07 PM
+-- Generation Time: Mar 06, 2025 at 01:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,6 +46,27 @@ INSERT INTO `oil_inventory` (`oil_id`, `oil_lit_type`, `amount`, `reg_date`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `pay_id` int(11) NOT NULL,
+  `pay_method` text DEFAULT NULL,
+  `pay_reg_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`pay_id`, `pay_method`, `pay_reg_date`) VALUES
+(1, 'CASH', '2025-03-06 07:21:00'),
+(2, 'CBE', '2025-03-06 07:22:14'),
+(3, 'bbbsss', '2025-03-06 10:47:09');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tanker_inventory`
 --
 
@@ -63,7 +84,8 @@ CREATE TABLE `tanker_inventory` (
 INSERT INTO `tanker_inventory` (`tanker_id`, `fuel_type`, `quantity_litter`, `added_date`) VALUES
 (2, 'Diesel', 1235, '2025-03-05 07:28:04'),
 (3, 'Gasoline', 7890, '2025-03-05 07:28:55'),
-(8, 'Benzin', 1, '2025-03-05 12:53:26');
+(8, 'Benzin', 1, '2025-03-05 12:53:26'),
+(10, 'CBE', NULL, '2025-03-06 07:21:17');
 
 -- --------------------------------------------------------
 
@@ -96,6 +118,12 @@ ALTER TABLE `oil_inventory`
   ADD PRIMARY KEY (`oil_id`);
 
 --
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`pay_id`);
+
+--
 -- Indexes for table `tanker_inventory`
 --
 ALTER TABLE `tanker_inventory`
@@ -118,10 +146,16 @@ ALTER TABLE `oil_inventory`
   MODIFY `oil_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `pay_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `tanker_inventory`
 --
 ALTER TABLE `tanker_inventory`
-  MODIFY `tanker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `tanker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
